34590d5170b7bfbd970cf76ace2132d0ec089e03
Nataliia <nataliiaqa8@gmail.com>
2024-10-10 17:45:02 +0200

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 import css from "./App.module.css";
 import { nanoid } from "nanoid";
 
-import ContactList from "./ContactList/ContactList";
-import SearchBox from "./SearchBox/SearchBox";
-import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactForm from "./components/ContactForm/ContactForm";
 
 const getInitialContacts = () => {
   const savedContacts = localStorage.getItem("contacts");
